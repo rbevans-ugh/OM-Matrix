991707804917e065cb698818496c040b41f42599
Rob Evans <robevans@microsoft.com>
2020-04-03 10:10:06 -0400

    Update addmobilefeature.js
    disabling flow

--- dist/js/addmobilefeature.js
+++ dist/js/addmobilefeature.js
@@ -11,7 +11,7 @@ function addFeature() {
     //url is hardcoded from flow. 
     console.log(data);
     var url =
-      "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
+    //   "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
     //clear old content out so we're not concating results on each run.
     fetch(url, {
       method: "POST",
