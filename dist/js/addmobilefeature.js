6524154d9db512f86baf9366349fb99931380b20
Rob Evans <robevans@microsoft.com>
2020-04-07 09:43:21 -0400

    OM Add Feature Work

--- dist/js/addmobilefeature.js
+++ dist/js/addmobilefeature.js
@@ -2,16 +2,25 @@ function addFeature() {
     console.log("hi");
     var confirm0 = document.getElementById("featureName").value;
     var select1 = document.getElementById("acctlist");
+    console.log(select1);
     var selected1 = []
-    for (var i = 0; i < select1.length; i++) {
-      if (select1.options[i].selected) selected1.push(select1.options[i].value);
+    var object = {}
+//     for (var i = 0; i < select1.length; i++) {
+//       if (select1.options[i].selected) 
+//       selected1.push(select1.options[i].value);
+//   }
+for (var i = 0; i < select1.length; i++) {
+    if (select1.options[i].selected)
+    object[select1.options[i].value] = selected1.push(select1.options[i].value);
+    console.log(selected1[i]);
   }
+  //var object = Object.assign({}, select1);
   console.log(selected1);
-    var data = { featureName: confirm0, selectedAccts: selected1};
+    var data = { featureName: confirm0, selectedAccts: object};
     //url is hardcoded from flow. 
     console.log(data);
     var url =
-    //   "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
+      "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
     //clear old content out so we're not concating results on each run.
     fetch(url, {
       method: "POST",
