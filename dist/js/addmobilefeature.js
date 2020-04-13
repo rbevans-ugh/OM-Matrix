48197a406b402b3e14fde7d6a50e141bfa66dd90
Rob Evans <robevans@microsoft.com>
2020-04-13 17:07:09 -0400

    adding confirmation\failure banner
    This will trigger if flow did or didn't get the request.

--- dist/js/addmobilefeature.js
+++ dist/js/addmobilefeature.js
@@ -1,4 +1,4 @@
-function addFeature() {
+function addFeature(flowStatus) {
     console.log("hi");
     var confirm0 = document.getElementById("featureName").value;
     var select1 = document.getElementById("acctlist");
@@ -16,11 +16,10 @@ for (var i = 0; i < select1.length; i++) {
   }
   //var object = Object.assign({}, select1);
   console.log(selected1);
-    var data = { featureName: confirm0, selectedAccts: object};
-    //url is hardcoded from flow. 
-    console.log(data);
-    var url =
-      "https://prod-19.westcentralus.logic.azure.com:443/workflows/8c948a552dea484196bb3275023e87b4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=a6__w3db5jkvbGJXIvT9G_YCDw3G2G7apfnfrR1OaxI";
+  var data = { featureName: confirm0, selectedAccts: object};
+  //url is hardcoded from flow. 
+  console.log(data);
+  var url ="https://prod-19.westcentralus.logic.azure.com:443/workflows/8c948a552dea484196bb3275023e87b4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=a6__w3db5jkvbGJXIvT9G_YCDw3G2G7apfnfrR1OaxI";
     //clear old content out so we're not concating results on each run.
     fetch(url, {
       method: "POST",
@@ -29,9 +28,20 @@ for (var i = 0; i < select1.length; i++) {
       },
       body: JSON.stringify(data)
     })
-      // .then(function(response) {
-      //   return response();
-      // });
-      console.log("got the response from Flow");
-      document.getElementById("addfeatureform").reset();
+    .then(function(response) {
+      var flowStatus = response.status;
+      console.log(response.status);
+      console.log("the flowstatus is " + flowStatus);
+      // return response.status();
+      console.log(flowStatus);
+      if (flowStatus == 200){
+        console.log("we should be here yo "+ flowStatus);
+        showConfirm("block");
+      }
+      else {
+        console.log("ruh roh "+ flowStatus);
+        showFail("block");
+      }
+    });
+    document.getElementById("addfeatureform").reset();
   }
