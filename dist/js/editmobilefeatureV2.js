a16c8701fe421e9137e09409d4146b9fe802ef38
Rob Evans <robevans@microsoft.com>
2020-04-20 21:31:59 -0400

    Update editmobilefeatureV2.js
    code for updating dashboard

--- dist/js/editmobilefeatureV2.js
+++ dist/js/editmobilefeatureV2.js
@@ -1,26 +1,13 @@
 function editFeature() {
     console.log("hi");
-    var confirm0 = document.getElementById("featureName").value;
-    var select1 = document.getElementById("acctlist");
-    console.log(select1);
-    var selected1 = []
-    var object = {}
-//     for (var i = 0; i < select1.length; i++) {
-//       if (select1.options[i].selected) 
-//       selected1.push(select1.options[i].value);
-//   }
-for (var i = 0; i < select1.length; i++) {
-    if (select1.options[i].selected)
-    object[select1.options[i].value] = selected1.push(select1.options[i].value);
-    console.log(selected1[i]);
-  }
-  //var object = Object.assign({}, select1);
-  console.log(selected1);
-    var data = { featureName: confirm0, selectedAccts: object};
+    var confirm0 = document.getElementById("dashboards").value;
+    var confirm1 = document.getElementById("id").value;
+    console.log(confirm0, confirm1);
+    var data = { dashboards: confirm0, id: confirm1};
     //url is hardcoded from flow. 
     console.log(data);
     var url =
-      "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
+     // "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
     //clear old content out so we're not concating results on each run.
     fetch(url, {
       method: "POST",
