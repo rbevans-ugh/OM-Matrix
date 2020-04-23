f9a686e449dfa9c82d1a99c0fab7c4fd449b43d6
Rob Evans <robevans@microsoft.com>
2020-04-23 14:51:20 -0400

    Update editmobilefeature.js
    complete unauth code

--- dist/js/editmobilefeature.js
+++ dist/js/editmobilefeature.js
@@ -12,30 +12,47 @@ function editFeature() {
       };
     }
     console.log("hi " + user);
-  var confirm0 = document.getElementById("featureName").value;
-  var checkboxes = document.getElementsByName("chkbox");
-  var accts = [];
-  var object = {};
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked == true)
-      object[checkboxes[i].id] = accts.push(checkboxes[i].id);
-  }
-  //var object = Object.assign({}, select1);
-  var data = { featureName: confirm0, selectedAccts: object, auth_user: user };
-  //url is hardcoded from flow.
-  console.log(data);
-  var url =
-    "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
-  //clear old content out so we're not concating results on each run.
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
-  console.log("got the response from Flow");
-  setTimeout(() => {
-    location.reload();
-  }, 1000);
+    var confirm0 = document.getElementById("featureName").value;
+    var checkboxes = document.getElementsByName("chkbox");
+    var accts = [];
+    var object = {};
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked == true)
+        object[checkboxes[i].id] = accts.push(checkboxes[i].id);
+    }
+    //var object = Object.assign({}, select1);
+    var data = { featureName: confirm0, selectedAccts: object, auth_user: user };
+    //url is hardcoded from flow.
+    console.log(data);
+    var url =
+      "https://prod-12.westcentralus.logic.azure.com:443/workflows/cfa4a35d41f045e2ba1bbeedda848c83/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AH4uTBll2IKhsiJ0UGRdkW1mnZBsnIiTgS68nhkUfEc";
+    //clear old content out so we're not concating results on each run.
+    if (user.includes("@icrosoft.com")) {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(function (response) {
+          var flowStatus = response.status;
+          console.log(response.status);
+          console.log("the flowstatus is " + flowStatus);
+          // return response.status();
+          if (flowStatus == 200) {
+            console.log("we should be here yo " + flowStatus);
+            setTimeout(() => {
+              location.reload();
+            }, 1000);
+          }
+          else {
+            console.log("ruh roh " + flowStatus);
+          }
+        });
+    } else {
+      showDeny("block");
+    }
+    document.getElementById("addfeatureform").reset();
+  })
 }
