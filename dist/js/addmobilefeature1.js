8458465fdf0fea2a1cea9ac3d585c6b064a33ed4
Rob Evans <robevans@microsoft.com>
2020-04-23 12:42:26 -0400

    Update addmobilefeature1.js

--- dist/js/addmobilefeature1.js
+++ dist/js/addmobilefeature1.js
@@ -2,21 +2,17 @@ function addFeature(flowStatus) {
 
   var user
 // Here we're getting the user making the submission
-  // $(document).ready(function () {
-    $.get("https://dev-omlc.azurewebsites.net/.auth/me", function (data, user) {
+    $.get("https://dev-omlc.azurewebsites.net/.auth/me", function (data) {
       for (var key in data[0]["user_claims"]) {
-        // var obj = data[0]["user_claims"][key];
+        var obj = data[0]["user_claims"][key];
         if (obj["val"].includes("microsoft")) {
           var user = obj["val"];
           console.log('I got the user ' + user)
           // We should have gotten the user now.
           break;
         };
-        // console.log(obj["typ"]); //claim type in user_claims
-        // console.log(obj["val"]); //claim value in user_claims
       }
     });
-  // })
   console.log("hi " + user);
   var confirm0 = document.getElementById("featureName").value;
   var checkboxes = document.getElementsByName('chkbox')
@@ -26,11 +22,12 @@ function addFeature(flowStatus) {
     if (checkboxes[i].checked == true)
       object[checkboxes[i].id] = accts.push(checkboxes[i].id);
   }
-  //var object = Object.assign({}, select1);
   var data = { featureName: confirm0, selectedAccts: object, user: user };
   //url is hardcoded from flow. 
   console.log(data);
+  // fake URL for testing
   var url = "https://prod-19.westcentralus.logic.azure.com:443/workflows/8c948a552dea484196bb3275023e87b4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=a6__w3db5jkvbGJXIvT9G_YCDw3G2G7apfnfrR1Oax";
+  // Real URL
   // var url = "https://prod-19.westcentralus.logic.azure.com:443/workflows/8c948a552dea484196bb3275023e87b4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=a6__w3db5jkvbGJXIvT9G_YCDw3G2G7apfnfrR1OaxI";
   //clear old content out so we're not concating results on each run.
   fetch(url, {
