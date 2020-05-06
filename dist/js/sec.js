a4cbad5e8f709d59b8bf6e7adb4a1018b4918606
Rob Evans <robevans@microsoft.com>
2020-05-06 08:04:52 -0400

    moving editaccess to another file

--- dist/js/sec.js
+++ dist/js/sec.js
@@ -23,29 +23,4 @@ function accessvalid() {
             table.hideColumn("editTableData")
         }
     });
-}
-
-function editaccess() {
-    // We're using this to prevent non-msft users from accessing this page and will redirect or post modal of access denied.
-    var user
-    // Here we're getting the user making the submission
-    // We can't always get claim/upn so relying on claim/name
-    $.get("https://dev-omlc.azurewebsites.net/.auth/me", function (data) {
-        for (var key in data[0]["user_claims"]) {
-            var obj = data[0]["user_claims"][key];
-            if (obj["typ"].includes("claims/name")) {
-                user = obj["val"];
-                if (user.includes("@microsoft.com")) {
-                    break;
-                }
-            }
-        };
-        if (user.includes("@microsoft.com")) {
-            console.log("We have a valid user.  Hi " + user)
-            table.hideColumn("editTableData")
-        } else {
-            console.log("We don't have a valid user");
-            // Mac matrix column
-        }
-    });
-}
+};
