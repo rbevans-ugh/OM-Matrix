5a58287667fb76353d6902b813355ecbd0cd23ae
Rob Evans <robevans@microsoft.com>
2020-05-07 16:08:22 -0400

    adding platform confirmation

--- dist/js/confirmmobilefeature.js
+++ dist/js/confirmmobilefeature.js
@@ -10,13 +10,22 @@ function confirmFeature(){
       accts.push(checkboxes[i].value)
     }
 }
- console.log("it worked "+ accts)
+    if (document.getElementById("ios").checked){
+      var varplat = "iOS"
+    }
+    if (document.getElementById("android").checked){
+      var varplat = "Android"
+    }
+    if (document.getElementById("bothplat").checked){
+      var varplat = "Both"
+    }
   // This is for matrix v2, but was breaking confirming feature work.
   // var confirm1 = document.getElementById("PM").value;
   // var confirm2 = document.getElementById("DEVOwner").value;
   // console.log(confirm0.options[confirm0.selectedIndex].value);
     console.log(confirm0);
   document.getElementById("confirmFeatureName").innerHTML = confirm0;
+  document.getElementById("confirmPlatform").innerHTML = varplat;
   document.getElementById("confirmaccttype").innerHTML = accts;
   }
   
