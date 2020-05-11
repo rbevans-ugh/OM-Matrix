5730695ae6024bd14a458ac01c4fbb0eb9ae6528
Rob Evans <robevans@microsoft.com>
2020-05-11 13:48:48 -0400

    adding support wiki field
    -- plumbing work confirm support wiki field
    -- adding support wiki field to add feature

--- dist/js/confirmmobilefeature.js
+++ dist/js/confirmmobilefeature.js
@@ -2,6 +2,7 @@ function confirmFeature(){
   showConfirm("none");
   showFail("none");
   var confirm0 = document.getElementById("featureName").value;
+  var confirm1 = document.getElementById("supportWiki").value;
   // Checking for account types checkbox
   var checkboxes = document.getElementsByName('chkbox')
   var accts = []
@@ -19,13 +20,10 @@ function confirmFeature(){
     if (document.getElementById("bothplat").checked){
       var varplat = "Both"
     }
-  // This is for matrix v2, but was breaking confirming feature work.
-  // var confirm1 = document.getElementById("PM").value;
-  // var confirm2 = document.getElementById("DEVOwner").value;
-  // console.log(confirm0.options[confirm0.selectedIndex].value);
     console.log(confirm0);
   document.getElementById("confirmFeatureName").innerHTML = confirm0;
   document.getElementById("confirmPlatform").innerHTML = varplat;
+  document.getElementById("confirmWiki").innerHTML = confirm1;
   document.getElementById("confirmaccttype").innerHTML = accts;
   }
   
